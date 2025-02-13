d76eac22c516233c8858efc818795db6631bb609
Yaroslav Malygin <ya.malygin@team.superjob.ru>
2025-02-13 18:47:01 +0300

    Initial Commit

---
+++ ContactsList/ContactsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsListApp.swift
+//  ContactsList
+//
+//  Created by Yaroslav Malygin on 13.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct ContactsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
